ad9b4c42ef6c45d6cd70082f4db48eb2435bfcc2
Simo067m <ssj.simonsj@gmail.com>
2024-11-18 15:03:00 +0100

    Make check_model_forward_script

---
+++ scripts/check_model_forward.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import torch
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from custom_transforms import LoadSpectrogram, NormalizeSpectrogram, ToTensor, InterpolateSpectrogram
+from data_management import make_dataset_name
+
+# CONSTANTS TO LEAVE
+DATA_ROOT = Path(f"/dtu-compute/02456-p4-e24/data") 
+ROOT = Path(__file__).parent.parent
+MODEL_DIR = ROOT / "models"
+STMF_FILENAME = "stmf_data_3.csv"
+NFFT = 512
+TS_CROPTWIDTH = (-150, 200)
+VR_CROPTWIDTH = (-60, 15)
+def check_model_forward(model):
+    BATCH_SIZE = 10
+    NUM_WORKERS = 10
+
+    dataset_name = make_dataset_name(nfft=NFFT, ts_crop_width=TS_CROPTWIDTH, vr_crop_width=VR_CROPTWIDTH)
+    data_dir = DATA_ROOT / dataset_name
+
+    TRAIN_TRANSFORM = transforms.Compose(
+        [LoadSpectrogram(root_dir=data_dir / "train"),
+        NormalizeSpectrogram(),
+        ToTensor(),
+        InterpolateSpectrogram()]
+    )
+
+    dataset_train = model.dataset(data_dir= data_dir / "train",
+                            stmf_data_path = DATA_ROOT / STMF_FILENAME,
+                            transform=TRAIN_TRANSFORM)
+    train_data_loader = DataLoader(dataset_train, 
+                                    batch_size=BATCH_SIZE,
+                                    shuffle=True,
+                                    num_workers=NUM_WORKERS)
+
+    sample_batch = train_data_loader.dataset[0]
+    spectogram = sample_batch["spectrogram"]
+    output = model(spectogram)
+    print(f"output : {output}")
+    print(f"output shape: {output.shape}")
+    return output
